1584946b0fa06898e08883b658eec4afbc38ab50
JustinGanzer <fleischgurke@gmail.com>
2015-11-30 21:53:16 +0100

    Server Client übernommen

--- Aufgabe02Client.java
+++ Aufgabe02Client.java
@@ -2,7 +2,44 @@
 public class Aufgabe02Client {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("C: Start");
+
+    // Socket einrichten und verbinden
+    // ---------------------------------------------------------
+    System.out.println("C: Socket einrichten und verbinden");
+    Socket s = new Socket("sun65.beuth-hochschule.de", 9021);
+
+    // Leser und Schreiber fuer den Socket einrichten
+    // ---------------------------------------------------------
+    BufferedReader sbr = new BufferedReader(
+                           new InputStreamReader(
+                             s.getInputStream() ));
+    PrintWriter spw = new PrintWriter(s.getOutputStream());
+
+    // Echostring vom Benutzer anfordern, schreiben und lesen
+    // ---------------------------------------------------------
+    String echo = null;
+    String back = null;
+    BufferedReader br = new BufferedReader(
+                            new InputStreamReader(System.in));
+    while(true) {
+      System.out.println("\nGib Echostring (Ende mit quit)");
+      System.out.print("->");
+      echo = br.readLine();
+      echo = echo.trim();
+      System.out.println("C: Vor dem println() mit:"+echo);
+      spw.println(echo);            // Schreiben und
+      spw.flush();
+      System.out.println("C: Vor dem readLine()");
+      back = sbr.readLine();        // Lesen
+      System.out.println("Zurueck kommt:" + back);
+      if(back.equals("quit")) break;
+    } // end while
+
+    spw.close();
+    sbr.close();
+    br.close();
+    s.close();
 
 	}
 
--- Aufgabe02Server.java
+++ Aufgabe02Server.java
@@ -1,8 +1,43 @@
+import java.io.*;
+import java.net.*;
 
 public class Aufgabe02Server {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int serverPort = 9021;
+    System.out.println("S: Start auf Host sun65" +
+                       " am Port "+serverPort);
+
+    // ServerSocket einrichten und im accept() warten
+    // ---------------------------------------------------------
+    ServerSocket ss = new ServerSocket(serverPort);
+    Socket        s = null;
+    System.out.println("S: Vor dem accept()");
+    s = ss.accept();
+
+    // Leser und Schreiber einrichten
+    // ---------------------------------------------------------
+    BufferedReader sbr = new BufferedReader(
+                           new InputStreamReader(
+                             s.getInputStream()));
+    PrintWriter spw = new PrintWriter(s.getOutputStream());
+
+    // Die wiederholten Echos mit diesem Client abwickeln
+    // ---------------------------------------------------------
+    String zeile;
+    while(true) {
+      System.out.println("S: Vor dem readLine()");
+      zeile = sbr.readLine();
+      System.out.println("S: Aus dem Socket kommt->" + zeile);
+      spw.println(zeile);
+      spw.flush();
+      if(zeile.equals("quit")) break;
+    } // while
+
+    spw.close();
+    sbr.close();
+    ss.close();
+    s.close();
 
 	}
 
